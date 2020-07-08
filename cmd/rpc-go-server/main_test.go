5a241031b9ea3fc8e706030bf70251716218857f
TJ Holowaychuk <tj@vision-media.ca>
2020-07-08 11:07:43 +0100

    refactor tests using assert.NoError()
    was fine as-is but I always use assert.NoError so might as well keep it consistent

--- cmd/rpc-go-server/main_test.go
+++ cmd/rpc-go-server/main_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"io/ioutil"
-	"log"
 	"testing"
 
 	"github.com/apex/rpc/schema"
@@ -12,40 +11,28 @@ import (
 
 func TestGenerateNoTypes(t *testing.T) {
 	exp, err := ioutil.ReadFile("fixtures/todo_no_types_gen.go.fix")
-	if err != nil {
-		t.Fatalf("error reading no types fixture: %v", err)
-	}
+	assert.NoError(t, err, "reading fixture")
 
 	schema, err := schema.Load("../../examples/todo/schema.json")
-	if err != nil {
-		log.Fatalf("error loading schema: %s", err)
-	}
+	assert.NoError(t, err, "loading schema")
 
 	var act bytes.Buffer
 	err = generate(&act, schema, "server", "", true)
-	if err != nil {
-		log.Fatalf("error generating: %v", err)
-	}
+	assert.NoError(t, err, "generating")
 
 	assert.Equal(t, string(exp), act.String())
 }
 
 func TestGenerateTypes(t *testing.T) {
 	exp, err := ioutil.ReadFile("fixtures/todo_types_gen.go.fix")
-	if err != nil {
-		t.Fatalf("error reading no types fixture: %v", err)
-	}
+	assert.NoError(t, err, "reading fixture")
 
 	schema, err := schema.Load("../../examples/todo/schema.json")
-	if err != nil {
-		log.Fatalf("error loading schema: %s", err)
-	}
+	assert.NoError(t, err, "loading schema")
 
 	var act bytes.Buffer
 	err = generate(&act, schema, "server", "github.com/apex/rpc/cmd/rpc-go-server/api", true)
-	if err != nil {
-		log.Fatalf("error generating: %v", err)
-	}
+	assert.NoError(t, err, "generating")
 
 	assert.Equal(t, string(exp), act.String())
 }
