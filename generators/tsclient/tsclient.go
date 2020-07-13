680dc937065fef7960fda003e81a6b38f47027e8
TJ Holowaychuk <tj@vision-media.ca>
2020-07-13 11:00:51 +0100

    fix TypeScript errors to support Deno
    for whatever reason Deno is more strict than ts-node, so a few
    things slipped through

--- generators/tsclient/tsclient.go
+++ generators/tsclient/tsclient.go
@@ -9,42 +9,61 @@ import (
 )
 
 var require = `
-let fetch = typeof window !== 'undefined' ? window.fetch : null
-if(!fetch) {
-  fetch = require('%s')
-}
+// fetch for Node
+const fetch = (typeof window == 'undefined' || window.fetch == null)
+// @ts-ignore
+  ? require('%s')
+  : window.fetch
 `
 
-var call = `
+var call = `/**
+ * ClientError is an API client error providing the HTTP status code and error type.
+ */
+
+class ClientError extends Error {
+  status: number;
+  type?: string;
+
+  constructor(status: number, message?: string, type?: string) {
+    super(message)
+    this.status = status
+    this.type = type
+  }
+}
+
 /**
-* Call method with params via a POST request.
-*/
+ * Call method with params via a POST request.
+ */
 
-async function call(url: string, authToken: string, method: string, params?: any): Promise<string> {
- const res = await fetch(url + '/' + method, {
-	 method: 'POST',
-	 body: JSON.stringify(params),
-	 headers: {
-		 'Content-Type': 'application/json',
-		 'Authorization': ` + "`Bearer ${authToken}`" + `
-	 }
- })
+async function call(url: string, method: string, authToken?: string, params?: any): Promise<string> {
+  const headers: Record<string, string> = {
+    'Content-Type': 'application/json'
+  }
+  
+  if (authToken != null) {
+    headers['Authorization'] = ` + "`Bearer ${authToken}`" + `
+  }
+  
+  const res = await fetch(url + '/' + method, {
+    method: 'POST',
+    body: JSON.stringify(params),
+    headers
+  })
 
- // we have an error, try to parse a well-formed json
- // error response, otherwise default to status code
- if (res.status >= 300) {
-	 let err
-	 try {
-		 const { type, message } = await res.json()
-		 err = new Error(message)
-		 err.type = type
-	 } catch {
-		 err = new Error(` + "`${res.status} ${res.statusText}`" + `)
-	 }
-	 throw err
- }
+  // we have an error, try to parse a well-formed json
+  // error response, otherwise default to status code
+  if (res.status >= 300) {
+    let err
+    try {
+      const { type, message } = await res.json()
+      err = new ClientError(res.status, message, type)
+    } catch {
+      err = new ClientError(res.status, res.statusText)
+    }
+    throw err
+  }
 
- return res.text()
+  return res.text()
 }`
 
 // Generate writes the TS client implementations to w.
@@ -63,7 +82,7 @@ func Generate(w io.Writer, s *schema.Schema, fetchLibrary string) error {
 	out(w, "export class Client {\n")
 	out(w, "\n")
 	out(w, "  private url: string\n")
-	out(w, "  private authToken: string\n")
+	out(w, "  private authToken?: string\n")
 	out(w, "\n")
 	out(w, "  /**\n")
 	out(w, "   * Initialize.\n")
@@ -111,18 +130,18 @@ func Generate(w io.Writer, s *schema.Schema, fetchLibrary string) error {
 			out(w, "    let res = ")
 			// call
 			if len(m.Inputs) > 0 {
-				out(w, "await call(this.url, this.authToken, '%s', params)\n", m.Name)
+				out(w, "await call(this.url, '%s', this.authToken, params)\n", m.Name)
 			} else {
-				out(w, "await call(this.url, this.authToken, '%s')\n", m.Name)
+				out(w, "await call(this.url, '%s', this.authToken)\n", m.Name)
 			}
 			out(w, "    let out: %sOutput = JSON.parse(res, this.decoder)\n", format.GoName(m.Name))
 			out(w, "    return out\n")
 		} else {
 			// call
 			if len(m.Inputs) > 0 {
-				out(w, "    await call(this.url, this.authToken, '%s', params)\n", m.Name)
+				out(w, "    await call(this.url, '%s', this.authToken, params)\n", m.Name)
 			} else {
-				out(w, "    await call(this.url, this.authToken, '%s')\n", m.Name)
+				out(w, "    await call(this.url, '%s', this.authToken)\n", m.Name)
 			}
 		}
 
