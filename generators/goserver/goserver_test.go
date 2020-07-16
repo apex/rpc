b860120f17e6946e83d0d7ab8fbb55690e532252
TJ Holowaychuk <tj@vision-media.ca>
2020-07-16 14:16:03 +0100

    add goserver test fixtures

--- generators/goserver/goserver_test.go
+++ generators/goserver/goserver_test.go
@@ -1,1 +1,33 @@
 package goserver_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/tj/assert"
+	"github.com/tj/go-fixture"
+
+	"github.com/apex/rpc/generators/goserver"
+	"github.com/apex/rpc/schema"
+)
+
+func TestGenerate_noTypes(t *testing.T) {
+	schema, err := schema.Load("../../examples/todo/schema.json")
+	assert.NoError(t, err, "loading schema")
+
+	var act bytes.Buffer
+	err = goserver.Generate(&act, schema, false, "")
+	assert.NoError(t, err, "generating")
+
+	fixture.Assert(t, "todo_server_no_types.go", act.Bytes())
+}
+func TestGenerate_types(t *testing.T) {
+	schema, err := schema.Load("../../examples/todo/schema.json")
+	assert.NoError(t, err, "loading schema")
+
+	var act bytes.Buffer
+	err = goserver.Generate(&act, schema, false, "api")
+	assert.NoError(t, err, "generating")
+
+	fixture.Assert(t, "todo_server_types.go", act.Bytes())
+}
---
+++ generators/goserver/testdata/todo_server_types.go
@@ -0,0 +1,65 @@
+// ServeHTTP implementation.
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  if r.Method == "GET" {
+    switch r.URL.Path {
+      case "/_health":
+        rpc.WriteHealth(w, s)
+      default:
+        rpc.WriteError(w, rpc.BadRequest("Invalid method"))
+    }
+    return
+  }
+
+  if r.Method == "POST" {
+    ctx := rpc.NewRequestContext(r.Context(), r)
+    var res interface{}
+    var err error
+    switch r.URL.Path {
+      case "/add_item":
+        var in api.AddItemInput
+        err = rpc.ReadRequest(r, &in)
+        if err != nil {
+          break
+        }
+        res, err = s.addItem(ctx, in)
+      case "/get_items":
+        res, err = s.getItems(ctx)
+      case "/remove_item":
+        var in api.RemoveItemInput
+        err = rpc.ReadRequest(r, &in)
+        if err != nil {
+          break
+        }
+        res, err = s.removeItem(ctx, in)
+      default:
+        err = rpc.BadRequest("Invalid method")
+    }
+
+    if err != nil {
+      rpc.WriteError(w, err)
+      return
+    }
+
+    rpc.WriteResponse(w, res)
+    return
+  }
+}
+
+// addItem Add an item to the list.
+func (s *Server) addItem(ctx context.Context, in api.AddItemInput) (interface{}, error) {
+  err := s.AddItem(ctx, in)
+  return nil, err
+}
+
+// getItems Return all items in the list.
+func (s *Server) getItems(ctx context.Context) (interface{}, error) {
+  res, err := s.GetItems(ctx)
+  return res, err
+}
+
+// removeItem removes an item from the to-do list.
+func (s *Server) removeItem(ctx context.Context, in api.RemoveItemInput) (interface{}, error) {
+  err := s.RemoveItem(ctx, in)
+  return nil, err
+}
+
