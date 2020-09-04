42684dd25e61956edc4e6c2f6a06a49b53b8df91
TJ Holowaychuk <tj@vision-media.ca>
2020-09-04 10:33:02 +0100

    add rpc.WriteResponse() benchmark

--- response_test.go
+++ response_test.go
@@ -11,6 +11,23 @@ import (
 	"github.com/apex/rpc"
 )
 
+// DiscardResponseWriter .
+type DiscardResponseWriter struct {
+}
+
+// Header implementation.
+func (d DiscardResponseWriter) Header() http.Header {
+	return http.Header{}
+}
+
+// Write implementation.
+func (d DiscardResponseWriter) Write([]byte) (int, error) {
+	return 0, nil
+}
+
+// WriteHeader implementation.
+func (d DiscardResponseWriter) WriteHeader(int) {}
+
 // Test responses.
 func TestWriteResponse(t *testing.T) {
 	t.Run("with no content", func(t *testing.T) {
@@ -30,4 +47,21 @@ func TestWriteResponse(t *testing.T) {
 		assert.Equal(t, 200, w.Code)
 		assert.Equal(t, "{\n  \"name\": \"Tobi\"\n}", strings.TrimSpace(w.Body.String()))
 	})
+}
+
+// Benchmark responses.
+func BenchmarkWriteResponse(b *testing.B) {
+	b.ReportAllocs()
+	b.SetBytes(1)
+
+	out := struct{ Name, Species, Email string }{
+		Name:    "Tobi",
+		Species: "Ferret",
+		Email:   "tobi@ferret.com",
+	}
+
+	var w DiscardResponseWriter
+	for i := 0; i < b.N; i++ {
+		rpc.WriteResponse(w, out)
+	}
 }
